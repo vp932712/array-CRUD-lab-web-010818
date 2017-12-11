792b6ea757062f24d725354f706caaf9bc8edcf1
Vaibhav Patel <vp932712@gmail.com>
2017-12-11 21:57:20 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,11 @@ array.shift(element)
 end
 
 def remove_element_from_end_of_array(array)
-
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+array.unshift
 end
 
 def retrieve_element_from_index(array, index_number)
